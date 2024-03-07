40afb2d770b1259bd559afc2f571ae81f6b7ef2b
LutinJr <72550004+PierreGourgouillon@users.noreply.github.com>
2024-03-07 10:15:08 +0100

    feature/Install-styled-components (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { Route, Routes } from "react-router-dom"
 import { NavBar } from "./components/navigationBar/navigation.bar"
 import HomePage from "./pages/home.page"
+import { lightTheme, darkTheme } from './theme';
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div className="h-screen w-screen flex">
-      <NavBar/>
-      <Routes>
-        <Route path="/home"  element={<HomePage/>}/>
-      </Routes>
-    </div>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <div className="h-screen w-screen flex">
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+// theme.ts
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  primaryColor: "#73db32",
+  secondaryColor: "#28a745",
+  textColor: "#000000"
+};
+
+export const darkTheme: DefaultTheme = {
+  primaryColor: "#007bff",
+  secondaryColor: "#28a745",
+  textColor: "#ffffff"
+};
