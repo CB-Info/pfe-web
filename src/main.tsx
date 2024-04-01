d5bda1968ce42a9521898ece451bdc0b923e45ab
LutinJr <72550004+PierreGourgouillon@users.noreply.github.com>
2024-04-02 00:00:07 +0200

    feature/create-pwa (#15)
    Co-authored-by: Clément Bilger <clement.bilger25@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import App from './App.tsx'
 import './applications/css/index.css'
 import { BrowserRouter } from 'react-router-dom';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
