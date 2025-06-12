6320bc624cbf819ce715a5e97a012507b0198723
Clément.B <72599289+CB-Info@users.noreply.github.com>
2025-06-12 16:52:05 +0200

    refactor: move button enums to separate file

---
+++ src/UI/components/buttons/custom.button.types.ts
@@ -0,0 +1,11 @@
+export enum TypeButton {
+    PRIMARY = "PRIMARY",
+    SECONDARY = "SECONDARY",
+    TEXT = "TEXT",
+}
+
+export enum WidthButton {
+    LARGE = "LARGE",
+    MEDIUM = "MEDIUM",
+    SMALL = "SMALL",
+}
